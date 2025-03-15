f07e79cd9fee5f1fc6bd7cfc5e4d6903bb531edb
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-15 10:35:27 +0545

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="main flex justify-center align-middle">
+      <h1 className='font-2xl'>hello world</h1>
+    </div>
+        
+    </>
+  )
+}
+
+export default App
