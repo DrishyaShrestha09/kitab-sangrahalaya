7abadc94b73978398d5580316ec12585a741efac
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-20 01:21:23 +0545

    successfully implemented register function using firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { AuthProvide } from "./context/AuthContext"; // ✅ Fixed import
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
-        <Outlet />
-      </main>
-      <Footer/>
+      <AuthProvide> {/* ✅ Use the correct provider name */}
+        <Navbar />
+        <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+          <Outlet />
+        </main>
+        <Footer />
+      </AuthProvide>
     </>
   );
 }
--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwAOhzyJMOSp2W1z39aB9JHzguX7CEQow",
-  authDomain: "book-store-mern-40a9d.firebaseapp.com",
-  projectId: "book-store-mern-40a9d",
-  storageBucket: "book-store-mern-40a9d.firebasestorage.app",
-  messagingSenderId: "496713394062",
-  appId: "1:496713394062:web:a3373fca0b8f88cb3566df"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID ,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
