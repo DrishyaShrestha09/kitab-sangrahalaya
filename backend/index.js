92fdb77f7d15e04704e9b2427e21f1460d0edc6c
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-19 09:26:43 +0545

    conneccted with the database and managed molders

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send("Hola Amigo!")
-})
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL)
+    app.get('/', (req, res) => {
+        res.send("Hola Amigo! Welcome to the book server!");
+    })
+}
+
+main() .then(()=> console.log("Successfully connected to MongoDB!")).catch(err => console.log(err));
 
 app.listen(port, () =>{
     console.log(`Server is listening on ${port}`)
